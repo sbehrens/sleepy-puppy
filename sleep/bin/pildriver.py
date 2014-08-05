240fe64bb253ea527ddd95dda7361a9c5994c3c1
Scott Behrens <sbehrens@netflix.com>
2014-08-05 15:04:33 -0700

    fixed init

--- sleep/bin/pildriver.py
+++
@@ -1,5 +0,0 @@
-#!/Users/sbehrens/dev/sleepy-puppy/sleep/bin/python
-# EASY-INSTALL-SCRIPT: 'Pillow==2.5.1','pildriver.py'
-__requires__ = 'Pillow==2.5.1'
-import pkg_resources
-pkg_resources.run_script('Pillow==2.5.1', 'pildriver.py')
