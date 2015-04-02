dfc27b34dac2589ed43c3413f72b150fd0e2e2e4
Scott Behrens <sbehrens@gmail.com>
2015-04-02 12:31:45 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,17 +9,18 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-            'Flask==0.10.1',
-            'Flask-SQLAlchemy==1.0',
-            'Flask-Script==0.6.3',
-            'SQLAlchemy',
-            'bcrypt',
-            'Flask-Admin',
-            'Flask-WTF',
-            'flask-bcrypt',
-            'Flask-RESTful',
-            'flask-login',
-            'Pillow',
-            'flask-mail',
-        ]
+    'Flask==0.10.1',
+    'Flask-Admin==1.0.7',
+    'Flask-Bcrypt==0.5.2',
+    'Flask-Login==0.2.9',
+    'Flask-Mail==0.9.0',
+    'Flask-RESTful==0.2.11',
+    'Flask-SQLAlchemy==1.0',
+    'Flask-Script==0.6.3',
+    'Flask-WTF==0.9.4',
+    'Pillow==2.7.0',
+    'SQLAlchemy==0.9.3',
+    'bcrypt==1.1.1',
+    'psycopg2'
+       ]
 )
