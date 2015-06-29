cecf051e7276b41812564e5ac2007156ed1830f4
Scott Behrens <sbehrens@gmail.com>
2015-06-29 13:13:26 -0700

    added more context for config settings

--- config-default.py
+++ config-default.py
@@ -4,21 +4,25 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = False
 
+# Security configuration settings
 SECRET_KEY = os.getenv('secret_key', 'ThiSISMYDARKSECRET!@#')
-
 CSRF_ENABLED = True
 CSRF_SESSION_KEY = os.getenv('csrf_session_key', 'ThiSISMYDARKSECRET!@#')
 
+# Database configuration settings
 SQLALCHEMY_DATABASE_URI = os.getenv('sleepypuppy_db', 'sqlite:////tmp/sleepy-db.db')
 
+# Screenshot storage configuration settings
 UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
 UPLOAD_SCREENSHOTS_TO_S3 = False
 S3_BUCKET = ""
 S3_FILES_PREFIX = "sleepypuppy"
 
+# Log configuration settings
 LOG_LEVEL = "DEBUG"
 LOG_FILE = "sleepypuppy.log"
 
+# Callback configuration settings for JS captures
 HOSTNAME = 'localhost' # for getting the JS file.
 CALLBACK_HOSTNAME = HOSTNAME
 CALLBACK_PROTOCOL = 'http' # http for local dev, https for deploy
@@ -32,9 +36,11 @@ SES_REGION = 'us-east-1'
 MAIL_SERVER = 'localhost'
 MAIL_PORT = 25
 MAIL_SENDER =  os.getenv('sender', 'sleepypuppy@domain.com')
+# Uncomment if your SMTP server requires authentication
 #MAIL_USE_TLS = False
 #MAIL_USE_SSL = False
 #MAIL_USERNAME = 'you'
 #MAIL_PASSWORD = 'your-password'
 
+# Captures will only be logged from the following list of domains
 ALLOWED_DOMAINS = ['localhost']
