ae5c8a047cddb037c9879a093ef7edc678a33953
Scott Behrens <sbehrens@netflix.com>
2015-08-17 18:51:41 -0700

    added netflix headers

--- sleepypuppy/admin/payload/views.py
+++ sleepypuppy/admin/payload/views.py
@@ -1,3 +1,16 @@
+#     Copyright 2015 Netflix, Inc.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
 import os
 from sleepypuppy import app, db
 from flask.ext.admin.contrib.sqla import ModelView
@@ -29,7 +42,6 @@ class Select2MultipleWidget(TextInput):
     def json_choices(field):
         objects = ('{{"id": {}, "text": "{}"}}'.format(*c)
                    for c in field.iter_choices())
-        print 'i am here'
         return '[' + ','.join(objects) + ']'
 
 
@@ -81,7 +93,6 @@ class Select2MultipleField(SelectMultipleField):
                 try:
                     self.data = []
                     for value in valuelist[0].split(','):
-                        print value
                         self.data.append(self.coerce(value))
                 except ValueError:
                     raise ValueError(
--- sleepypuppy/templates/loader.js
+++ sleepypuppy/templates/loader.js
@@ -1,3 +1,17 @@
+/*     Copyright 2015 Netflix, Inc.
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
 console.log("Sleepy Puppy is a Cross-site Scripting Payload Management Framework")
 console.log("Sleepy Puppy JavaScripts will execute in the order they were configured, but may finish execution at different times depending on if the JavaScripts are asyncronous.")
 console.log("More information on Sleepy Puppy can be found here: https://github.com/Netflix/sleepy-puppy")
@@ -5,7 +19,7 @@ console.log("More information on Sleepy Puppy can be found here: https://github.
 if (typeof jQuery === 'undefined') {
 
     function getScript(url, success) {
-        var script     = document.createElement('script');
+        var script = document.createElement('script');
         script.src = url;
 
         var head = document.getElementsByTagName('head')[0],
